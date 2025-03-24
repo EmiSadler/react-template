44890a23c801ccc124b4847fe5456b0fedb49ebe
Emily Sadler <emily@sadler.gg>
2025-03-24 16:20:15 +0000

    Project set up and first changes

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+
+
+const Button = (props) => {
+    return <h1 id="Button">{props.label}</h1>;
+};
+
+
+
+export default Button;
---
+++ src/components/Gig.jsx
@@ -0,0 +1,19 @@
+import './Gig.css';
+
+function Gig() {
+  return (
+    <div>
+      <h1>Event</h1>
+      <img id="AlbumCover"
+        src="https://upload.wikimedia.org/wikipedia/en/5/5b/The_Decemberists_-_The_King_Is_Dead.jpg" alt="The Decemberists Album Cover: The King is Dead" />
+      <p id="Event">
+        A chill evening listening to The Decemberists with custom themed cocktails. </p>
+      <p id="Date">
+        31 May 2025</p>
+      <p id="Location">
+        Somewhere cozy with a fire</p>
+    </div>
+  );
+}
+
+export default Gig
